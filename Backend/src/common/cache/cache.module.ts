e4121375cebda4ee34d27f289b6dbc418f545413
Haymisey <167537434+Haymisey@users.noreply.github.com>
2025-01-12 10:54:50 +0300

    Delete Backend

--- Backend/src/common/cache/cache.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { CacheService } from "./cache.service";
-
-@Module({
-  imports: [CacheService],
-  providers: [CacheService],
-})
-export class CacheModule {}
